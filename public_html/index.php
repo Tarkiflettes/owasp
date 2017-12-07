f8190e33b8dbfa0bee5b116ed5b36655ef8a5b95
Théophile <demaegdt@et.esiea.fr>
2017-12-07 23:33:47 +0100

    DEV - Création du menu

--- public_html/index.php
+++ public_html/index.php
@@ -44,7 +44,9 @@ require_once __DIR__ . '/../db_config.php';
             nom : admin<br/>
             mot de passe : ' or '1=1<br/>
             <br/>
-            Le mot de passe de base était pourtant : Admn1str@t3ur
+            Le mot de passe de base était pourtant : Admn1str@t3ur<br/>
+            <br/>
+            Il est également possible de se connecter en mettant ' or '1=1 au lieu de l'identifiant admin
         </div>
     </body>   
 </html>
